bb77d39905ca136d74a7bfd7311ea335fe1ececa
Viegros <moisandt@gmail.com>
2026-05-19 17:16:01 +0200

    Update TeleOp.java.java

--- TeleOp.java.java
+++ TeleOp.java.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-
+@TeleOp 
 
 public class TeleOp extends LinearOpMode {
 
